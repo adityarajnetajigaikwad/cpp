6a3a7c8eff10b78d1b2681004812ede7f0139ddb
adityarajnetajigaikwad <165874447+adityarajnetajigaikwad@users.noreply.github.com>
2024-05-04 15:03:26 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int b,h
+float area;
+printf("enter the base length");
+scanf("%d",&b);
+printf("enter the height length");
+scanf("%d",&h);
+area= 0.5*b*h;
+printf("area of triangle is %f",area);
+}
