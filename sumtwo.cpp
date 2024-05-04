b729225ee95080542bcbbac18870c75781832052
adityarajnetajigaikwad <165874447+adityarajnetajigaikwad@users.noreply.github.com>
2024-05-04 15:11:56 +0530

    Add files via upload

---
+++ sumtwo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//using namespace std
+int main()
+{
+	int a,b;
+	std::cout<<"enter the number 1:\n";
+	std::cin>>a;
+	std::cout<<"enter the number 2:\n";
+	std::cin>>b;
+	int sum=a+b;
+	std::cout<<"sum of no.:\n"<<sum;
+}
